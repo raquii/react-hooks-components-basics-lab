48c9a423d31c42ee0b74114d55e3bc39ef1d5878
raquel <raquelanaroman@gmail.com>
2021-06-12 16:11:28 -0400

    commit tree 8c098c2216e7d595f79dead36ba0b223e92e4583

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About(){
+    return(
+        <div id='about'>
+            <p>This is my about element</p>
+        </div>
+    )
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
+import NavBar from './NavBar'
+import Home from './Home'
+import About from './About'
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
